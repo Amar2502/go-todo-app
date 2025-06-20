cc918244a98d6ca3a151988c87af4038c313d15f
Amar2502 <amarpandey2502@gmail.com>
2025-06-20 14:19:07 +0530

    made config file and implemented it in main.go

---
+++ cmd/go-todo-app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/Amar2502/go-todo-app/internal/config"
+)
+
+func main() {
+
+	//load config
+	cfg := config.MustLoad()
+
+	//database setup
+
+	//router setup
+	
+
+	//server setup
+
+	//gracefully shutting down server
+
+}
---
+++ internal/config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HttpServer struct {
+	Addr string `yaml:"address" env:"address" env-required:"true"`
+}
+
+type Config struct {
+	env          string     `yaml:"env" env:"env" env-required:"true"`
+	storage_path string     `yaml:"storage_path" env:"storage_path" env-required:"true"`
+	HttpServer   HttpServer `yaml:"http_server"`
+}
+
+func MustLoad() *Config {
+
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to configuration file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("config path is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("config file does not exist: ", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		log.Fatal("cannot read config file:", err.Error())
+	}
+
+	return &cfg
+
+}
